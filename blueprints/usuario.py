256d0b7447d72409d7a7cdc024584bdcadd6b155
Lucas R. de Oliveira <77167403+lucas1528@users.noreply.github.com>
2024-06-26 12:21:50 -0300

    att

--- blueprints/usuario.py
+++ blueprints/usuario.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort, request
+from flask import Blueprint, redirect, render_template, abort, request, url_for, session
 import requests
 from jinja2 import TemplateNotFound
 
@@ -34,6 +34,102 @@ def adicionar_usuario():
             }
             
             response = requests.post('http://127.0.0.1:8000/info/create', json=info)
-            print(response.json())
         
         return render_template('adicionar_usuario.html')
+    
+
+@usuario.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        if session.get('usuario'):
+            return redirect(url_for('usuario.cpis_aluno'))
+        return render_template('login.html')
+    else:
+        data = {
+            "login": request.form.get('login'),
+            "senha": request.form.get('senha'),
+        }
+        
+        response = requests.post('http://127.0.0.1:8000/usuario/login', json=data)
+
+        if response.status_code == 200:
+            usuario = response.json()
+            session['usuario'] = usuario['id']
+            
+            return redirect(url_for('usuario.cpis_aluno'))
+        
+        return render_template('login.html')
+    
+
+@usuario.route('/logout', methods=['GET'])
+def logout():
+    del session['usuario']
+    return redirect(url_for('login'))
+
+
+@usuario.route('/cpis_aluno', methods=['GET'])
+def cpis_aluno():
+    if not session.get('usuario'):
+        return redirect(url_for('usuario.login'))
+
+    response = requests.get(f'http://127.0.0.1:8000/cpi/get_all_aluno?usuario_id={session["usuario"]}')
+    
+    if response:
+        cpis = response.json()
+    else:
+        cpis = []   
+    
+    
+    return render_template('home.html', cpis=cpis)
+
+
+@usuario.route('/cpis_comunicante', methods=['GET'])
+def cpis_comunicante():
+    if not session.get('usuario'):
+        return redirect(url_for('usuario.login'))
+
+    response = requests.get(f'http://127.0.0.1:8000/cpi/get_all_comunicante?usuario_id={session["usuario"]}')
+    
+    if response:
+        cpis = response.json()
+    else:
+        cpis = []   
+    
+    
+    return render_template('home.html', cpis=cpis)
+
+
+@usuario.route('/cpis_chefe_de_curso', methods=['GET'])
+def cpis_chefe_de_curso():
+    if not session.get('usuario'):
+        return redirect(url_for('usuario.login'))
+
+    response = requests.get(f'http://127.0.0.1:8000/cpi/get_all_chefe_de_curso?usuario_id={session["usuario"]}')
+    
+    if response:
+        cpis = response.json()
+    else:
+        cpis = []   
+        
+    
+    return render_template('home.html', cpis=cpis)
+
+
+@usuario.route('/cpis_comandante', methods=['GET'])
+def cpis_comandante():
+    if not session.get('usuario'):
+        return redirect(url_for('usuario.login'))
+
+    response = requests.get(f'http://127.0.0.1:8000/cpi/get_all_comandante?usuario_id={session["usuario"]}')
+    
+    if response:
+        cpis = response.json()
+    else:
+        cpis = []   
+        
+    
+    return render_template('home.html', cpis=cpis)
+    
+    
+        
+    
